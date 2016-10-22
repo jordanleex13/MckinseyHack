18b259510cea19ce450f3c64d7e51c9448bc1d2d
Jordan <jordandlee@hotmail.ca>
2016-10-22 12:47:08 -0400

    JSONParser added

---
+++ MckinseyHack/MckinseyHack/JSONParser.swift
@@ -0,0 +1,16 @@
+//
+//  JSONParser.swift
+//  MckinseyHack
+//
+//  Created by Jordan Lee on 2016-10-22.
+//  Copyright © 2016 Jordan Lee. All rights reserved.
+//
+
+import Foundation
+
+class JSONParser {
+
+    func downloadCoordinates() {
+        let url = NSURL(string: <#T##String#>)
+    }
+}
